4428ed55feca387884b452b529c956683a7dc25b
josh-white <joshua.jay.white@gmail.com>
2021-11-04 14:57:26 -0500

    initial push for battleship setup

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import BattleShip from './BattleShip';
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(<BattleShip />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
