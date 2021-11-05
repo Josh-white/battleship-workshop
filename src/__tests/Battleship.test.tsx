f800dc4233996d6a1598fcdfe3fd322293d84b6d
josh-white <joshua.jay.white@gmail.com>
2021-11-05 12:13:05 -0500

    worked on tests with rich for battleship homepage

---
+++ src/__tests/Battleship.test.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import {BattleShip} from "../BattleShip";
+import {fireEvent,render, screen} from "@testing-library/react";
+
+
+
+describe('Battleship', () => {
+  it('should contain a background image', () => {
+    render(<BattleShip/>)
+    expect(screen.getByRole('img', {name: 'battleship logo'})).toBeVisible()
+  })
+
+  it('should contain a button to select single player',  () => {
+    render(<BattleShip/>)
+
+    expect(screen.getByRole('button', {name: /One Player/i})).toBeVisible()
+  });
+
+  it('should render a different page when one player button is clicked',  () => {
+    render(<BattleShip/>)
+
+    fireEvent.click(screen.getByRole('button', {name: /one player/i}))
+
+
+  });
+
+})
